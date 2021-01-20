ee66e0a59affeb801f1c7bb3ea3d9ecd28b93f7a
Joseph-R5 <joseph.rullan@hotmail.com>
2021-01-20 11:29:30 +0000

    Error handling refactor

--- anime-recommender/src/middleware/index.js
+++ anime-recommender/src/middleware/index.js
@@ -16,17 +16,15 @@ import {
 export const checkIfAnimeAlreadyExistsMiddleware = store => next => action => {
   switch (action.type) {
     case ADD_ANIME:
-      // TODO
-      // Error handler here if no such exists (test with Anime: Moldiver)
       const animeTitleSearch = action.animeResults.title;
       const animeTitleList = store.getState().animeListReducer.animeTitleList;
 
       if (animeTitleList.includes(animeTitleSearch)) {
-        const payload = ANIME_ALREADY_EXISTS
+        const errorMessage = ANIME_ALREADY_EXISTS
         const load = false;
 
         next({ type: LOAD_ANIME_LIST_SPINNER, load })
-        return next({ type: LOAD_ERROR_MSG, payload })
+        return next({ type: LOAD_ERROR_MSG, errorMessage })
       }
       break;
     case FIND_RECOMMENDATIONS:
@@ -37,8 +35,8 @@ export const checkIfAnimeAlreadyExistsMiddleware = store => next => action => {
     case DELETE_ANIME:
       const animeTitleListCount = store.getState().animeListReducer.animeTitleList.length;
       if (animeTitleListCount === 1) {
-        const payload = EMPTY_ANIME_LIST;
-        return next({ type: LOAD_ERROR_MSG, payload })
+        const errorMessage = EMPTY_ANIME_LIST;
+        return next({ type: LOAD_ERROR_MSG, errorMessage })
       }
       break;
     default:
